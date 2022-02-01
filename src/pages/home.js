b0d3b2c66b136866e8d3af38058c76e80fbdfdbd
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-01 14:29:05 -0600

    Add per to total

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { continentsList } from '../redux/home/home';
 
+const population = 7924303275;
 const Home = () => {
   const total = useSelector((state) => state.home.total);
   return (
@@ -12,17 +13,39 @@ const Home = () => {
           <p>
             total:
             <br />
+            Population:
+            {' '}
+            {population.toLocaleString()}
+            <br />
             confirmed:
             {' '}
             {total.today_confirmed.toLocaleString()}
+            <br />
+            Per Population:
+            {((total.today_confirmed / population) * 100)
+              .toFixed(2)
+              .toLocaleString()}
+            %
             <br />
             deaths:
             {' '}
             {total.today_deaths.toLocaleString()}
             <br />
+            Per Population:
+            {((total.today_deaths / population) * 100)
+              .toFixed(2)
+              .toLocaleString()}
+            %
+            <br />
             new:
             {' '}
             {total.today_new_confirmed.toLocaleString()}
+            <br />
+            Per Population:
+            {((total.today_new_confirmed / population) * 100)
+              .toFixed(2)
+              .toLocaleString()}
+            %
           </p>
         )}
       </div>
