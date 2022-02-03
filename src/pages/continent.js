33b0131af85c0691aff019b899ec8bdf8ab39bb4
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-03 10:53:57 -0600

    Implement filter

--- src/pages/continent.js
+++ src/pages/continent.js
@@ -1,7 +1,7 @@
 import { React, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { fetchCountries } from '../redux/home/home';
+import { fetchCountries, filterAction } from '../redux/home/home';
 import { corresponding, pop } from '../redux/home/staticData';
 import ContinentDiv from '../components/continent-div';
 import './continent.css';
@@ -15,20 +15,31 @@ const Continent = () => {
     dispatch(fetchCountries(href));
   }, []);
 
+  const filterStr = useSelector((state) => state.home.filter);
   const countries = useSelector((state) => state.home.countries);
   const data = useSelector((state) => state.home.data);
   const countriesFiltered = countries
     .map((country) => (corresponding[country.name]
       ? { ...country, name: corresponding[country.name] }
       : { ...country }))
-    .filter((country) => data[country.name]);
+    .filter((country) => data[country.name])
+    .filter((country) => country.name.toUpperCase().indexOf(filterStr.toUpperCase()) > -1);
 
   return (
     <section className="countries">
       <h1>{continent}</h1>
       <div className="search-bar">
-        <input type="text" className="search input" placeholder="Search by country" name="filter-input" />
-        <button type="button" className="search btn">&#128269;</button>
+        <input
+          type="text"
+          className="search input"
+          placeholder="Search by country"
+          name="filter-input"
+          value={filterStr}
+          onChange={(e) => dispatch(filterAction(e.target.value))}
+        />
+        <button type="button" className="search btn">
+          &#128269;
+        </button>
       </div>
       <div className="countries tabs">
         {countriesFiltered.map((country) => (
