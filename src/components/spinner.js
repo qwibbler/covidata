bd4614172b5850abef8fecdff7318d5e2103ccfd
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-03 13:22:09 -0600

    Implement spinner

---
+++ src/components/spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => (
+  <h2 style={{ textAlign: 'center' }}>
+    <div className="spin" />
+  </h2>
+);
+export default Spinner;
